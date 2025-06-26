90fe5fd5c14435a0433b41af47ffc4134f455c81
niraihan <raihan14un@gmail.com>
2025-06-26 17:38:07 +0600

    Plant care upgading on going 1

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
         <div className="carousel w-full h-[300px] md:h-[450px] rounded-lg shadow-md mb-10">
             {/* Slide 1 */}
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/kVy6xzMM/Banner-1.jpg" className="w-full object-cover" alt="Care Tips 1" />
+                <img src="https://i.ibb.co/KpggVrd8/ignacio-correia-1-yycyo-MT6g-unsplash.jpghttps://i.ibb.co/kVy6xzMM/Banner-1.jpg" className="w-full object-cover" alt="Care Tips 1" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#000000b6] to-transparent w-full p-6 md:p-12 text-white">
                     <div className="max-w-md">
                         <h2 className="text-2xl md:text-4xl font-bold mb-2">Water Smartly</h2>
@@ -20,7 +20,7 @@ const Banner = () => {
 
             {/* Slide 2 */}
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/RGMT2HC7/banner-2.webp" className="w-full object-cover" alt="Care Tips 2" />
+                <img src="https://i.ibb.co/qMbXqypx/mark-raptapolus-l-Xt-VJ6n-P9lc-unsplash.jpg" className="w-full object-cover" alt="Care Tips 2" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#000000b6] to-transparent w-full p-6 md:p-12 text-white">
                     <div className="max-w-md">
                         <h2 className="text-2xl md:text-4xl font-bold mb-2">Sunlight Matters</h2>
@@ -37,7 +37,7 @@ const Banner = () => {
 
             {/* Slide 3 */}
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/XkrdKTc6/banner3.jpg" className="w-full object-cover" alt="Care Tips 3" />
+                <img src="https://i.ibb.co/M5gzBtKb/veronica-reverse-q-Ywy-RF9u-uo-unsplash.jpg" className="w-full object-cover" alt="Care Tips 3" />
                 <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#000000b6] to-transparent w-full p-6 md:p-12 text-white">
                     <div className="max-w-md">
                         <h2 className="text-2xl md:text-4xl font-bold mb-2">Check Soil Moisture</h2>
